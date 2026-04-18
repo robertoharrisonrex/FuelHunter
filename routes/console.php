5e424c660093c9eb4750eaa96f5f82d6ac808f10
Roberto <roberto.harrison@rexsoftware.com.au>
2026-04-18 10:26:05 +1000

    seo: schedule sitemap:generate weekly

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('migrate:fresh --seed')->dailyAt('01:00');
+Schedule::command('sitemap:generate')->weekly();
