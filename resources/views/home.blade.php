1ba36b6a5c73715d3457fe3e46810e8ccd9429fa
Roberto <roberto.harrison@rexsoftware.com.au>
2026-04-18 10:21:54 +1000

    seo: add meta tags and WebSite JSON-LD to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,24 @@
-<x-layout :full-bleed="true">
+<x-layout
+    :full-bleed="true"
+    :seo="[
+        'title'       => 'Queensland Fuel Prices Map | FuelHunter',
+        'description' => 'Live fuel prices across Queensland. Find the cheapest petrol, diesel and LPG near you.',
+    ]"
+>
     <x-slot:heading></x-slot:heading>
+
+    <x-slot:head>
+        @php
+        $schema = [
+            '@context'    => 'https://schema.org',
+            '@type'       => 'WebSite',
+            'name'        => 'FuelHunter',
+            'url'         => config('app.url'),
+            'description' => 'Track live fuel prices across Queensland.',
+        ];
+        @endphp
+        <script type="application/ld+json">{!! json_encode($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}</script>
+    </x-slot:head>
+
     <livewire:fuel-map />
 </x-layout>
